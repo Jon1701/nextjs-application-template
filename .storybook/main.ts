5747bda6b6c25a99713074b1361060446fe03084
Jon Balon <development@jonbalon.com>
2023-01-08 17:46:32 -0500

    Configure Storybook to look for Stories outside of `stories/`

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,7 @@
 import type { StorybookConfig } from '@storybook/core-common';
 
 const config: StorybookConfig = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
